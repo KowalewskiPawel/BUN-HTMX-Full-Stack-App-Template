2c8d362fd1ab1f0801c17912fc1032563c920929
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-09-15 13:11:39 +0300

    add dev script

--- index.ts
+++ index.ts
@@ -2,4 +2,6 @@ import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello from BUN!").listen(3000);
 
-console.log(`Server running at http://${app.server?.hostname}:${app.server?.port}`);
+console.log(
+  `Server running at http://${app.server?.hostname}:${app.server?.port}`
+);
