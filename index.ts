60c04ca4b816a0014fa50a2baa6be2a002fb46d1
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-09-15 13:08:58 +0300

    add hello world from Elysia and run script

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello from BUN!").listen(3000);
+
+console.log(`Server running at http://${app.server?.hostname}:${app.server?.port}`);
