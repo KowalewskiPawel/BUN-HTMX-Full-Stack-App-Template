445951d0bface7ec791adb4a4811db64a8885ccb
KowalewskiPawel <pawelkowalewsk@gmail.com>
2023-09-16 19:31:08 +0300

    add client side scripting comment

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,6 @@
 import * as elements from "typed-html";
 
+// _="on submit target.reset()" is the client-side scripting part, for resetting the input field after adding new todo item
 export const TodoForm = () => (
     <form class="flex flex-row space-x-3" hx-post="/todos" hx-swap="beforebegin" _="on submit target.reset()">
       <input type="text" name="content" class="border border-black" />
